91e862a8cb9d774a4613f87895504e8fa85e9971
johan <jeramos61@ucatolica.edu.co>
2024-05-08 20:35:20 -0500

    v4 se ajusta lso services de gamaproducto y producto

--- Jardineria/src/main/java/com/example/Jardineria/ModuloAuditoria/ServiceLog/AuditoriaService.java
+++ Jardineria/src/main/java/com/example/Jardineria/ModuloAuditoria/ServiceLog/AuditoriaService.java
@@ -1,11 +1,11 @@
-package com.example.Jardineria.ModuloAuditoria.ServiceLog;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Service
 public class AuditoriaService {
@@ -28,11 +28,15 @@ public class AuditoriaService {
     private String errorLogFilePath;
 
     // Método para registrar errores
-    public void registrarError(String mensaje) {
+    public void registrarError(Date timestamp, String errorType, String errorMessage) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String formattedTimestamp = dateFormat.format(timestamp);
+        String logMessage = "Timestamp: " + formattedTimestamp + ", Error Type: " + errorType + ", Error Message: " + errorMessage;
+
         try {
             FileWriter fileWriter = new FileWriter(errorLogFilePath, true); // true para agregar al archivo existente
             PrintWriter printWriter = new PrintWriter(fileWriter);
-            printWriter.println("Error: " + mensaje);
+            printWriter.println(logMessage);
             printWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -40,6 +44,3 @@ public class AuditoriaService {
         }
     }
 }
-
-
-
